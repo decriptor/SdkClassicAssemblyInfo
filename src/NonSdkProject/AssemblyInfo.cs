4fc0a26424ab186405100876ec6b2808549fea75
Stephen Shaw <sshaw@novarad.net>
2024-07-29 18:06:23 -0600

    Initial working POC

---
+++ src/NonSdkProject/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+[assembly: AssemblyTitle("My Assembly Title")]
+[assembly: AssemblyDescription("My Assembly Description")]
---
+++ src/SdkProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Get the executing assembly
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        // Get all custom attributes
+        var attributes = assembly.GetCustomAttributes(false);
+
+        foreach (var attribute in attributes)
+        {
+            Type attrType = attribute.GetType();
+
+            // Display the attribute type
+            Console.WriteLine($"Attribute: {attrType.Name}");
+
+            // Get all properties of the attribute
+            var properties = attrType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                // Get the property name and value
+                string name = property.Name;
+                object value = property.GetValue(attribute) ?? "null";
+
+                // Display the property name and value
+                Console.WriteLine($"{name}: {value}");
+            }
+
+            // Add a blank line for readability between attributes
+            Console.WriteLine();
+        }
+    }
+}
